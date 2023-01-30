377f5a340889524ba01c79c6a7ac4a5dbc0e6a50
JoySystem_v <fvv2011@gmail.com>
2023-01-30 14:28:13 +0400

    added contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
-    extend: {},
+    screens:{
+      sm: '320px',
+      md: '792px',
+      lg: '1200px',
+      xl: '1440px',
+    },
+    extend: {
+
+      backgroundImage: {
+        
+        'footer-texture': "url('../src/img/gradient.jpg')",
+      },
+      colors: {
+        lavander: {
+          500: '#7E3AF2;',
+          800: '#E02424;',
+        },
+        black: {
+          500: '#27272A;',
+        },
+        white: {
+          500: '#FCFBFA;',
+        },
+        gray: {
+          500: '#E5E7EB;',
+          800: '#696969;',
+        },
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+
+
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
